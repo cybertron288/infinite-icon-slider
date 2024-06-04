b45c830cde741035bd8b82ebfce72d39f73cd492
Ravi Patel <ravi60353@gmail.com>
2024-06-04 14:21:34 +0530

    app component update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import './App.css'
 import SliderDesign from './components/Slider';
+import {
+  FigmaLogoIcon,
+  FramerLogoIcon,
+  SketchLogoIcon,
+  TwitterLogoIcon,
+  GitHubLogoIcon,
+  VercelLogoIcon,
+  NotionLogoIcon,
+  DiscordLogoIcon,
+  InstagramLogoIcon,
+  LinkedInLogoIcon,
+} from "@radix-ui/react-icons";
+
+const icons = [
+  { key:"figma", icon: <FigmaLogoIcon /> },
+  { key:"twitter", icon: <TwitterLogoIcon /> },
+  { key:"github", icon: <GitHubLogoIcon /> },
+  { key:"instagram", icon: <InstagramLogoIcon /> },
+  { key:"linkedin", icon: <LinkedInLogoIcon /> },
+  { key:"discord", icon: <DiscordLogoIcon /> },
+  { key:"sketch", icon: <SketchLogoIcon /> },
+  { key:"notion", icon: <NotionLogoIcon /> },
+  { key:"vercel", icon: <VercelLogoIcon /> },
+  { key:"framer", icon: <FramerLogoIcon /> },
+];
 
 
 function App() {
+
+
   return (
     <>
-      <SliderDesign />
+    <div className='bg-black'>
+      <SliderDesign icons={icons} styles={{"width": "50%"}} />
+    </div>
     </>
   );
 }
